bd679dfb75ac0e44a57111a32002a48d0eb3b423
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-08-29 17:39:32 -0400

    commit tree c41be600ccd955ae14c901718463581f76850c55

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,20 @@
 export default (state = [], action) => {
-  return state;
+  switch(action.type){
+    case 'ADD_QUOTE':
+      state = state.concat(action.quote)
+      return state
+    case 'REMOVE_QUOTE':
+      state = state.filter(quote => quote.id !== action.quoteId)
+      return state
+    case 'UPVOTE_QUOTE':
+      const quoteIndex = state.findIndex(quote => quote.id === action.quoteId)
+      const quote = state[quoteIndex]
+      return [...state.slice(0, quoteIndex), {...quote, votes: quote.votes += 1}, ...state.slice(quoteIndex + 1)]
+    case 'DOWNVOTE_QUOTE':
+      const quoteIndex = state.findIndex(quote => quote.id === action.quoteId)
+      const quote = state[quoteIndex]
+      return [...state.slice(0, quoteIndex), {...quote, votes: quote.votes -= 1}, ...state.slice(quoteIndex + 1)]
+    default:
+      return state
+  }
 }
